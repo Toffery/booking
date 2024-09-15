f9bda2064c5ab0f1792e5fa00759aa2ffbce852e
toffery <loltoffery@gmail.com>
2024-09-16 00:58:50 +0300

    feat: added paginaation to endpoint get_hotels

--- routers/hotels.py
+++ routers/hotels.py
@@ -5,6 +5,15 @@ from schemas.hotels import Hotel, HotelPUT
 hotels = [
     {"id": 1, "title": "Sochi", "description": "Hotel in sochi"},
     {"id": 2, "title": "Moscow", "description": "Hotel in moscow"},
+    {"id": 3, "title": "Berlin", "description": "Hotel in berlin"},
+    {"id": 4, "title": "London", "description": "Hotel in london"},
+    {"id": 5, "title": "Paris", "description": "Hotel in paris"},
+    {"id": 6, "title": "Rome", "description": "Hotel in rome"},
+    {"id": 7, "title": "New York", "description": "Hotel in new york"},
+    {"id": 8, "title": "Tokyo", "description": "Hotel in tokyo"},
+    {"id": 9, "title": "Sydney", "description": "Hotel in sydney"},
+    {"id": 10, "title": "Amsterdam", "description": "Hotel in amsterdam"},
+    {"id": 11, "title": "Amman", "description": "Hotel in amman"},
 ]
 
 router = APIRouter(prefix="/hotels")
@@ -14,8 +23,17 @@ async def root():
     return {"message": "Hello from FastAPI"}
 
 @router.get("/hotels")
-async def get_hotels():
-    return hotels
+async def get_hotels(
+    page: int|None = 1,
+    per_page: int|None = 5
+):
+    start = (page - 1) * per_page
+    end = start + per_page
+    return {
+        "page": page,
+        "per_page": per_page,
+        "hotels": hotels[start:end]
+    }
 
 @router.post("/hotels")
 async def create_hotel(hotel: Hotel):
