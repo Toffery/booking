8bb98e2669a7292da9064c0df19865c9afffa6fa
toffery <loltoffery@gmail.com>
2024-11-28 10:23:49 +0300

    feat: added basic redis commands

---
+++ src/connectors/redis_connector.py
@@ -0,0 +1,26 @@
+import redis.asyncio as redis
+
+class RedisConnector:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.redis = None
+
+    async def connect(self):
+        self.redis = await redis.Redis(host=self.host, port=self.port)
+
+    async def set(self, key: str, value: str, exp: int = None):
+        if exp:
+            await self.redis.set(key, value, exp)
+        else:
+            await self.redis.set(key, value)
+
+    async def get(self, key: str):
+        return await self.redis.get(key)
+
+    async def delete(self, key):
+        await self.redis.delete(key)
+
+    async def close(self):
+        if self.redis:
+            await self.redis.aclose()
