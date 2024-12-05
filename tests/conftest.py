da1b730d2064f51976b39f8346b0e125c39a53ba
Toffery <loltoffery@gmail.com>
2024-12-05 17:55:27 +0300

    test: added two generators, ac and db, that are making easy working with DBManager and AsyncClient

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,28 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 import httpx
 import json
 
 from src.main import app
+from src.utils.db_manager import DBManager
+
+
+@pytest.fixture(
+    scope="function",
+)
+async def db() -> DBManager:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
+
+@pytest.fixture(
+    scope="session"
+)
+async def ac() -> AsyncClient:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        yield ac
 
 
 @pytest.fixture(
@@ -26,35 +43,35 @@ async def setup_database():
     scope="session",
     autouse=True
 )
-async def insert_hotels_and_rooms(setup_database):
+async def insert_hotels_and_rooms(setup_database, ac):
     with open("tests/mock_hotels.json") as f1, open("tests/mock_rooms.json") as f2:
         hotels = json.load(f1)
         rooms = json.load(f2)
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        for hotel in hotels:
-            response = await ac.post("/hotels/", json=hotel)
-            assert response.status_code == 200, ("Failed to add hotels in db", response.status_code, response.text)
-        for room in rooms:
-            hotel_id = room["hotel_id"]
-            print(room)
-            response = await ac.post(
-                f"/hotels/{hotel_id}/rooms",
-                json=room
-            )
-            assert response.status_code == 200, ("Failed to add rooms in db", response.status_code, response.text)
+    for hotel in hotels:
+        response = await ac.post("/hotels/", json=hotel)
+        assert response.status_code == 200, ("Failed to add hotels in db", response.status_code, response.text)
+    for room in rooms:
+        hotel_id = room["hotel_id"]
+        response = await ac.post(
+            f"/hotels/{hotel_id}/rooms",
+            json=room
+        )
+        assert response.status_code == 200, ("Failed to add rooms in db", response.status_code, response.text)
+
+
+
 
 
 @pytest.fixture(
     scope="session",
     autouse=True
 )
-async def add_user(setup_database):
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.post(
-            "/auth/signup",
-            json={
-                "email": "test@ya.ru",
-                "password": "test",
-            }
-        )
-        assert response.status_code == 200
+async def add_user(setup_database, ac):
+    response = await ac.post(
+        "/auth/signup",
+        json={
+            "email": "test@ya.ru",
+            "password": "test",
+        }
+    )
+    assert response.status_code == 200
--- tests/hotels/test_db.py
+++ tests/hotels/test_db.py
@@ -3,13 +3,12 @@ from src.hotels.schemas import HotelCreateOrUpdate
 from src.utils.db_manager import DBManager
 
 
-async def test_create_hotel():
+async def test_create_hotel(db):
     hotel_data = HotelCreateOrUpdate(
         title="Test hotel",
         location="Test location"
     )
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        ret_hotel = await db.hotels.add(hotel_data=hotel_data)
-        await db.commit()
+    ret_hotel = await db.hotels.add(hotel_data=hotel_data)
+    await db.commit()
 
-        assert ret_hotel
+    assert ret_hotel
