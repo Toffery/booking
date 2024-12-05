0e4a5d10801ff05898f937b69112cf932711a531
Toffery <loltoffery@gmail.com>
2024-12-05 17:38:02 +0300

    test: small refactoring

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,18 +30,18 @@ async def insert_hotels_and_rooms(setup_database):
     with open("tests/mock_hotels.json") as f1, open("tests/mock_rooms.json") as f2:
         hotels = json.load(f1)
         rooms = json.load(f2)
-        async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-            for hotel in hotels:
-                response = await ac.post("/hotels/", json=hotel)
-                assert response.status_code == 200, ("Failed to add hotels in db", response.status_code, response.text)
-            for room in rooms:
-                hotel_id = room["hotel_id"]
-                print(room)
-                response = await ac.post(
-                    f"/hotels/{hotel_id}/rooms",
-                    json=room
-                )
-                assert response.status_code == 200, ("Failed to add rooms in db", response.status_code, response.text)
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        for hotel in hotels:
+            response = await ac.post("/hotels/", json=hotel)
+            assert response.status_code == 200, ("Failed to add hotels in db", response.status_code, response.text)
+        for room in rooms:
+            hotel_id = room["hotel_id"]
+            print(room)
+            response = await ac.post(
+                f"/hotels/{hotel_id}/rooms",
+                json=room
+            )
+            assert response.status_code == 200, ("Failed to add rooms in db", response.status_code, response.text)
 
 
 @pytest.fixture(
