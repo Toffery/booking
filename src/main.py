4b3dfa9d60e4385c49a15941d4bf46064ab41727
toffery <loltoffery@gmail.com>
2024-12-29 18:29:48 +0300

    style: added python emoji in fastapi title

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ async def lifespan(app: FastAPI):
     print("Redis connection closed")
 
 
-app = FastAPI(title="Learning FastAPI", lifespan=lifespan)
+app = FastAPI(title="Learning FastAPI 🐍", lifespan=lifespan)
 
 app.include_router(router_auth)
 app.include_router(router_bookings)
